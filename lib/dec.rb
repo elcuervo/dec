22fcda83efd559834bd66faa6a8ebcbeb4a2fe42
elcuervo <elcuervo@elcuervo.net>
2021-03-19 12:27:59 -0300

    Freeze constants

--- lib/dec.rb
+++ lib/dec.rb
@@ -1,38 +1,42 @@
+# frozen_string_literal: true
+
 module Dec
   HASH_ARRAY = -> (h, k) { h[k] = [] }
+  NS = "_dec_".freeze
 
   def _executor
-    @_executor ||= Hash.new(&HASH_ARRAY)
+    @_dec_executor ||= Hash.new(&HASH_ARRAY)
   end
 
   def dec_stack
-    @_stack ||= []
+    @_dec_stack ||= []
   end
 
   def dec_calls
-    @_calls ||= []
+    @_dec_calls ||= []
   end
 
   def method_added(method_sym)
-    return if dec_calls.include?(method_sym) || dec_stack.empty?
+    return if method_sym.to_s.start_with?(NS) || dec_calls.include?(method_sym) || dec_stack.empty?
 
     dec_calls << method_sym
 
-    _executor[method_sym] << instance_method(method_sym)
-    _executor[method_sym] << dec_stack.select { |fn| fn.respond_to?(:call) }
+    alias_method :"#{NS}#{method_sym}", method_sym
+
+    _executor[method_sym] = dec_stack.select { |fn| fn.respond_to?(:call) }.reverse
 
     class_eval <<-RB, __FILE__, __LINE__ + 1
       def #{method_sym}(*args, **kwargs, &block)
-         original, stack = self.class._executor[__method__]
+        result = send(:#{NS}#{method_sym}, *args, *kwargs, &block)
+        stack = self.class._executor[:#{method_sym}]
 
-         result = original.bind(self).call(*args, *kwargs, &block)
-         stack.reverse.each { |action| result = action.call(result) }
-         result
+        stack.each { |action| result = action.call(result) }
+        result
       end
     RB
   end
 
   def dec(*args, &block)
-    @_stack = dec_stack + args << block
+    @_dec_stack = dec_stack + args << block
   end
 end
